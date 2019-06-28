798ef4ecc70f893629576e62e862e133f5218b71
Renzo <rcnavarrop@gmail.com>
2019-06-28 10:03:42 -0500

    adding react preset to babelrc

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,14 +11,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
       }
     ]
   },
   resolve: {
-    extensions: ['*', '.js']
+    extensions: ['*', '.js', '.jsx']
   },
 
   plugins: [
