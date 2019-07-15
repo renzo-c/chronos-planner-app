e11ef671238022a32bc7225de8010dd97875307d
Renzo <rcnavarrop@gmail.com>
2019-07-15 15:35:30 -0500

    testing

--- src/assets/Components/NavBarMenu/index.jsx
+++ src/assets/Components/NavBarMenu/index.jsx
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../../constants/routes';
 import SignOutButton from '../../../components/SignOut';
-import style from '../../../styles/navBarMenu.css';
+import style from '../../../styles/NavBarMenu';
 import { AuthUserContext } from '../../../components/Session';
 
 const NavBarMenu = () => {
