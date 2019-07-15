dad0b5158b9097fc68e542aa5b37a61f62e784e8
Renzo <rcnavarrop@gmail.com>
2019-07-14 23:09:44 -0500

    adding npm path for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
