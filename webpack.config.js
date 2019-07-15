f02467523df7ac499bc53cd189435c74c424968c
Renzo <rcnavarrop@gmail.com>
2019-07-14 19:07:26 -0500

    creating webpack.config file for common use

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+
+  node: {
+    fs: 'empty'
+  },
+  
+  entry: "./src/index.jsx",
+  
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  plugins: [
+    new Dotenv(),
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'Chronos Planner',
+        template: './src/indexTemplate.html'
+    })
+  ],
+
+  output: {
+    path: __dirname + "/public",
+    publicPath: "",
+    filename: "bundle.js"
+  },
+
+  devServer: {
+    // For index.html. Static files are served from url pointed to by contentBase (e.g: index.html)
+    contentBase: "./public",
+    // For bundle.js. Html page <script> tags are pointing to the in-memory bundle, which is served at url pointed to by publicPath 
+    publicPath: "/",
+    watchContentBase: true,
+    compress: true,
+    port: 3000,
+    hot: true,
+    historyApiFallback: true
+  }
+};
