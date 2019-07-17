d2f6d9a11012b6565cd26448e3b14065123a85f9
Renzo <rcnavarrop@gmail.com>
2019-07-17 15:38:42 -0500

    using webpack.DefinePluging in webpack to pass process.env object to react app

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const webpack = require('webpack')
 // var UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+// const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   devtool: 'source-map',
@@ -38,7 +39,16 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   plugins: [
-    new Dotenv(),
+    // new Dotenv(),
+    new webpack.DefinePlugin({
+      'process.env': {
+         'YOUR_API_KEY': JSON.stringify(process.env.YOUR_API_KEY),
+         'YOUR_AUTH_DOMAIN': JSON.stringify(process.env.YOUR_AUTH_DOMAIN),
+         'YOUR_DATABASE_URL': JSON.stringify(process.env.YOUR_DATABASE_URL),
+         'YOUR_PROJECT_ID': JSON.stringify(process.env.YOUR_PROJECT_ID),
+         'YOUR_MESSAGING_SENDER_ID': JSON.stringify(process.env.YOUR_MESSAGING_SENDER_ID),
+      }
+    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
